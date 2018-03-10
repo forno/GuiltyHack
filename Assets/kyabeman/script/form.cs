c42e1976c5cdde756a085f131d06876017f88cda
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-03-10 17:52:38 +0900

    First product

---
+++ Assets/kyabeman/script/form.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class form : MonoBehaviour {
+
+	public GameObject yakkaip;
+	public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+		GameObject prefav = (GameObject)Instantiate (yakkaip);
+		prefav.transform.SetParent (canvas.transform, false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/kyabeman/script/prefavForm.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefavForm : MonoBehaviour {
+	public float spawn_interval;
+
+	public GameObject instance;
+	public GameObject YakkaiP;
+
+	private IEnumerator coroutine;
+
+
+	void Start () {
+		coroutine = instant();
+		StartCoroutine (coroutine);
+	}
+
+	private IEnumerator instant(){
+		while (true) {
+			GameObject prefav = (GameObject)Instantiate (YakkaiP, instance.transform.position, Quaternion.identity);
+			prefav.transform.SetParent (instance.transform, false);
+			yield return new WaitForSeconds (spawn_interval);
+		}
+	}
+}
